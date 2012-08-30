eeccc7bb03d0f26e3f7ca2ae5756841a6aa98814
unknown <jdutton@C-L-2287.education2020.com>
2012-08-30 10:41:24 -0700

    Initial commit of PrepWebLog

---
+++ PrepWebLog/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace PrepWebLog
+{
+	class Program
+	{
+		static int Main(string[] args)
+		{
+			if (args.Length < 2)
+			{
+				Console.Write("Usage: PrepWebLog.exe [InputFile] [OutputFile]\n");
+				return -1;
+			}
+
+			try
+			{
+				string path = args[0];
+				using (FileStream sourceFile = File.OpenRead(args[0]), destinationFile = File.Create(args[1]))
+				using (StreamReader sourceFileReader = new StreamReader(sourceFile))
+				using (StreamWriter destinationFileWriter = new StreamWriter(destinationFile))
+				{
+					string line;
+					while ((line = sourceFileReader.ReadLine()) != null)
+					{
+						// remove any lines that start with #
+						if (line[0] != '#')
+							destinationFileWriter.WriteLine(line);
+					}
+					destinationFileWriter.Close();
+					sourceFileReader.Close();
+					destinationFile.Close();
+					sourceFile.Close();
+				}
+			}
+			catch (Exception e)
+			{
+				Console.Write(string.Format("Could not process the file. Error: {0}\nStack Trace: {1}", e.Message, e.StackTrace));
+				return -1;
+			}
+			return 0;
+		}
+	}
+}
